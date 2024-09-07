34c819660bad152ac6e2b123e61739269ba8f46d
Vidal.jrs <160967256+Vidaljrs@users.noreply.github.com>
2024-09-06 21:36:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+  {
+    titulo: "Blonde",
+    descricao: "Blonde é o segundo álbum de estúdio de Frank Ocean, lançado em 2016. Considerado por muitos críticos e fãs como uma obra-prima da música contemporânea, o álbum explora temas como amor, perda, identidade e sexualidade de forma profunda e poética.",
+    Link: "https://open.spotify.com/intl-pt/artist/2h93pZq0e7k5yf4dywlkpM?flow_ctx=1e99efda-e190-4ccc-b959-94846a8b74b0%3A1725520888",
+  },
+  {
+    titulo: "channel ORANGE",
+    descricao: "Channel Orange é o álbum de estreia do cantor norte-americano Frank Ocean, lançado em 17 de julho de 2012. Considerado um dos melhores álbuns da década de 2010, o disco recebeu aclamação da crítica e do público, consolidando Frank Ocean como uma das principais vozes da música contemporânea.",
+    Link: "https://open.spotify.com/intl-pt/artist/2h93pZq0e7k5yf4dywlkpM?flow_ctx=1e99efda-e190-4ccc-b959-94846a8b74b0%3A1725520888"
+  },
+  {
+    titulo: "Novacane",
+    descricao: "Novacane é uma das primeiras canções de Frank Ocean a ganhar destaque, lançada em seu EP homônimo em 2011. A faixa, com sua batida R&B suave e letras introspectivas, rapidamente se tornou um hino para aqueles que buscavam uma conexão mais profunda com a música.",
+    Link: "https://open.spotify.com/intl-pt/artist/2h93pZq0e7k5yf4dywlkpM?flow_ctx=1e99efda-e190-4ccc-b959-94846a8b74b0%3A1725520888"
+  },
+  {
+    titulo: "Endless",
+    descricao: "Endless é um projeto visual e musical lançado por Frank Ocean em 2016, que serviu como uma espécie de ponte entre seu álbum de estreia 'channel ORANGE' e o aclamado 'Blonde'.",
+    Link: "https://open.spotify.com/intl-pt/artist/2h93pZq0e7k5yf4dywlkpM?flow_ctx=1e99efda-e190-4ccc-b959-94846a8b74b0%3A1725520888"
+  },
+  {
+    titulo: "Noby",
+    descricao: "Noby é uma faixa instrumental de Frank Ocean que faz parte do seu projeto visual de 2016, 'Endless'. A música, caracterizada por uma atmosfera introspectiva e melancólica, típica do trabalho de Ocean, apresenta batidas minimalistas e samples que criam uma atmosfera hipnotizante.'",
+    Link: "https://open.spotify.com/intl-pt/artist/2h93pZq0e7k5yf4dywlkpM?flow_ctx=1e99efda-e190-4ccc-b959-94846a8b74b0%3A1725520888"
+  },
+];
+
